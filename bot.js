891f8e74defe4fac1959ae54a75345b04e7b360d
King9969 <71065155+King9969@users.noreply.github.com>
2020-10-06 00:03:03 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,14 @@ const { prefix, token, } = require('./config.json')
 const client = new Discord.Client()
 const balance = []
 
+
+const getquote = () => {
+  const api = require('./quotes.json')
+  let rand = Math.floor(Math.random() * 1600)
+  q = api[rand]
+
+}
+
 client.once("ready", () => {
   console.log("ready")
   client.user.setActivity("WITH GAAND", { type: "PLAYING" })
@@ -13,17 +21,17 @@ client.on('message', message => {
 
 
   if (message.content.startsWith(`${prefix}dedo`)) {
-    let rand = Math.floor(Math.random() * 100000) + 10;
+    let rand = Math.floor(Math.random() * 10000) + 10;
     const x = rand
 
     balance.push(rand)
 
-    message.channel.send("You got " + x.toLocaleString() + " coins for the server")
+    message.channel.send("You got " + "**" + x.toLocaleString() + "**" + " coins for the server")
   }
   else if (message.content.startsWith(`${prefix}bal`)) {
 
     sum = eval(balance.join('+'))
-    message.channel.send("SERVERS NET WORTH IS " + sum.toLocaleString())
+    message.channel.send("SERVERS NET WORTH IS " + "**" + sum.toLocaleString() + "**")
   }
   else if (message.content.startsWith(`${prefix}img`)) {
     let rand = Math.floor(Math.random() * 100000) + 1000;
@@ -32,7 +40,23 @@ client.on('message', message => {
   }
   else if (message.content.startsWith(`${prefix}tt`)) {
 
-    message.channel.send("https://ibb.co/ynSN6zp")
+    message.channel.send("https://ibb.co/DDBpMk3")
+
+  }
+  else if (message.content.startsWith(`${prefix}quo`)) {
+
+    getquote()
+    message.channel.send("**" + q.text + "**")
+
+  }
+  else if (message.content.startsWith(`${prefix}help`)) {
+
+    getquote()
+    message.channel.send("**" + " r dedo  -- to get free coins " + "**")
+    message.channel.send("**" + " r img  -- to get random image " + "**")
+    message.channel.send("**" + " r bal  -- show servers balance " + "**")
+    message.channel.send("**" + " r tt  -- to show timetable " + "**")
+    message.channel.send("**" + " r quo  -- to show random quote " + "**")
 
   }
 })
