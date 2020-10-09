2f26c12843b438cf61066ec33ab0e175cb7eb476
King9969 <71065155+King9969@users.noreply.github.com>
2020-10-09 08:49:49 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ client.on('message', message => {
     message.channel.send("**" + a + "**")
   }
   let tt = () => {
-    message.channel.send("https://i.ibb.co/X80M5Cc/pp.jpg")
+    message.channel.send("https://i.ibb.co/Ws6KhpM/pp.jpg")
   }
   let img = () => {
     let rand = Math.floor(Math.random() * 100000) + 1000;
