bbc067429d73a6b7f2e333dba998a4bb4e4b7198
King9969 <71065155+King9969@users.noreply.github.com>
2020-10-10 14:04:08 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const client = new Discord.Client()
 const rohit = [7777777]
 const CharlesLeemon = [214213]
 const pritam = [100440]
-const dankmemer = [564554]
+const jupiterfest = [564554]
 const Climax2147 = [34341]
 const JackHeroYO = [54544]
 const shauny = [243252]
@@ -89,7 +89,7 @@ client.on('message', message => {
       pritam.push(rand)
       rand = Math.floor(Math.random() * 100000) + 1500;
       message.channel.send("dank memer " + "**" + rand.toLocaleString() + "**")
-      dankmemer.push(rand)
+      jupiterfest.push(rand)
       rand = Math.floor(Math.random() * 100000) + 1500;
       message.channel.send("Climax2147 " + "**" + rand.toLocaleString() + "**")
       Climax2147.push(rand)
@@ -438,23 +438,23 @@ client.on('message', message => {
     if (message.content.toLowerCase().startsWith(`${prefix}beg`)) {
       let rand = Math.floor(Math.random() * 10000) + 10;
       const ra = rand
-      dankmemer.push(rand)
+      jupiterfest.push(rand)
       message.channel.send("You got " + "**" + ra.toLocaleString() + "**" + " coins for  yourself")
     }
     else if (message.content.toLowerCase().startsWith("bet " + luc)) {
       let rand = Math.floor(Math.random() * 100000) + 100;
-      dankmemer.push(rand)
+      jupiterfest.push(rand)
       message.channel.send("won " + "**" + rand + "**" + "coins")
       luc = Math.floor(Math.random() * 10) + 1;
     }
     else if (message.content.toLowerCase().startsWith("bet ")) {
       let rand = Math.floor(Math.random() * 1000) + 10;
-      dankmemer.push(-rand)
+      jupiterfest.push(-rand)
       message.channel.send("lost " + "**" + rand + "**" + " coins " + " (pick no between 1 to 10 )")
       luc = Math.floor(Math.random() * 10) + 1;
     }
     else if (message.content.toLowerCase().startsWith(`${prefix}bal`)) {
-      sum = eval(dankmemer.join('+'))
+      sum = eval(jupiterfest.join('+'))
       message.channel.send("**" + message.author.username + "**" + " NET WORTH IS " + "**" + sum.toLocaleString() + "**")
     }
     else if (message.content.toLowerCase().startsWith(`${prefix}img`)) {
